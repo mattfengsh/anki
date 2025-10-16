b00f8cf5470f6eadd2a6e41044872e4747089444
mattfengsh <mattfengsh@hotmail.com>
2025-10-17 04:52:18 +0800

    Update mod.rs

--- rslib/src/storage/card/mod.rs
+++ rslib/src/storage/card/mod.rs
@@ -839,7 +839,7 @@ impl fmt::Display for ReviewOrderSubclause {
                 let next_day_at = timing.next_day_at.0;
                 let now = timing.now.0;
                 temp_string =
-                    format!("if(extract_fsrs_variable(data, 's')<6, 0, 1), if(extract_fsrs_variable(data, 's')<6, 0,round(extract_fsrs_relative_retrievability(data, case when odue !=0 then odue else due end, {today}, ivl, {next_day_at}, {now})*3))  {order}, ord, extract_fsrs_variable(data, 'd') asc, extract_fsrs_variable(data, 's') desc");
+                    format!("if(extract_fsrs_variable(data, 's')<6, 0, 1),  ord, round(extract_fsrs_relative_retrievability(data, case when odue !=0 then odue else due end, {today}, ivl, {next_day_at}, {now})*3)  {order}, extract_fsrs_variable(data, 's') asc, extract_fsrs_variable(data, 'd') asc");
                 &temp_string
             }
             ReviewOrderSubclause::Added => "nid asc, ord asc",
